54967594c468f748652168e37b7fa469fef8b990
YaroslavAntoniuk <i23-1912-701-01@lute.lviv.ua>
2024-04-01 22:18:47 +0300

    Update MenuPage with the shadowDOM & a template

--- components/MenuPage.js
+++ components/MenuPage.js
@@ -1,6 +1,14 @@
 export class MenuPage extends HTMLElement {
   constructor() {
     super();
+
+    this.root = this.attachShadow({ mode: 'open' });
+  }
+
+  connectedCallback() {
+    const template = document.getElementById('menu-page-template');
+    const content = template.content.cloneNode(true);
+    this.root.appendChild(content);
   }
 }
 
