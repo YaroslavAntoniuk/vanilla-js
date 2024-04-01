96ff6ed665f12b2a64fa8d1fee91c6304fc1e844
YaroslavAntoniuk <i23-1912-701-01@lute.lviv.ua>
2024-04-01 18:24:20 +0300

    Updated Router services to play as a real router

--- services/Router.js
+++ services/Router.js
@@ -9,12 +9,47 @@ const Router = {
         Router.go(route);
       });
     });
+
+    // Event handler for url changes
+    window.addEventListener('popstate', (event) => {
+      Router.go(event.state.route, false);
+    });
+
+    // Check the initial URL
     Router.go(location.pathname);
   },
   go: (route, addToHistory = true) => {
     if (addToHistory) {
       history.pushState({ route }, '', route);
     }
+
+    let pageElement = null;
+    switch (route) {
+      case '/':
+        pageElement = document.createElement('h1');
+        pageElement.textContent = 'Menu';
+        break;
+
+      case '/order':
+        pageElement = document.createElement('h1');
+        pageElement.textContent = 'Your Order';
+        break;
+      default:
+        if (route.startsWith('/product/')) {
+          pageElement = document.createElement('h1');
+          pageElement.textContent = 'Details';
+          const paramId = route.substring(route.lastIndexOf('/') + 1);
+          pageElement.dataset.id = paramId;
+        }
+    }
+
+    if (!pageElement) return;
+
+    const main = document.querySelector('main');
+    main.children[0]?.remove();
+    main.appendChild(pageElement);
+    window.scrollX = 0;
+    window.scrollY = 0;
   },
 };
 
