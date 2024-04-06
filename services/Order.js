364916addde916d77195c6bbd40927cc7b09f0eb
YaroslavAntoniuk <i23-1912-701-01@lute.lviv.ua>
2024-04-06 18:35:17 +0300

    Added  order service

---
+++ services/Order.js
@@ -0,0 +1,25 @@
+import { getProductById } from './Menu.js';
+
+export async function addToCart(id) {
+  const product = await getProductById(id);
+  const results = app.store.cart.filter(
+    (prodInCart) => prodInCart.product.id === id,
+  );
+
+  if (results.length) {
+    app.store.cart = app.store.cart.map((prodInCart) => {
+      if (prodInCart.product.id === id) {
+        return { ...prodInCart, quantity: prodInCart.quantity + 1 };
+      }
+      return prodInCart;
+    });
+  } else {
+    app.store.cart = [...app.store.cart, { product, quantity: 1 }];
+  }
+}
+
+export function removeFromCard(id) {
+  app.store.cart = app.store.cart.filter(
+    (prodInCart) => prodInCart.product.id !== id,
+  );
+}
